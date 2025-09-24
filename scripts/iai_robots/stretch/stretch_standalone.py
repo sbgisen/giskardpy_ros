1a50884628263cf5dbdd142e6418ffe97c5586fe
jsupratman13 <supratmanjoshua@gmail.com>
2025-09-24 11:11:33 +0900

    fix sample bug

--- scripts/iai_robots/stretch/stretch_standalone.py
+++ scripts/iai_robots/stretch/stretch_standalone.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import rospy
 
-from giskardpy.configs.behavior_tree_config import StandAloneBTConfig
-from giskardpy.configs.giskard import Giskard
-from giskardpy.configs.iai_robots.stretch import StretchCollisionAvoidanceConfig, StretchStandaloneInterface
-from giskardpy.configs.iai_robots.tiago import TiagoCollisionAvoidanceConfig, TiagoStandaloneInterface
-from giskardpy.configs.world_config import WorldWithDiffDriveRobot
+from giskardpy_ros.configs.behavior_tree_config import StandAloneBTConfig
+from giskardpy_ros.configs.giskard import Giskard
+from giskardpy_ros.configs.iai_robots.stretch import StretchCollisionAvoidanceConfig, StretchStandaloneInterface
+from giskardpy_ros.configs.iai_robots.tiago import TiagoCollisionAvoidanceConfig, TiagoStandaloneInterface
+from giskardpy.model.world_config import WorldWithDiffDriveRobot
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
-    giskard = Giskard(world_config=WorldWithDiffDriveRobot(),
+    giskard = Giskard(world_config=WorldWithDiffDriveRobot(urdf=rospy.get_param('robot_description')),
                       collision_avoidance_config=StretchCollisionAvoidanceConfig(),
                       robot_interface_config=StretchStandaloneInterface(),
                       behavior_tree_config=StandAloneBTConfig(publish_tf=True, publish_js=True, debug_mode=True))
